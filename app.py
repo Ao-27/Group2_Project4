483247ffec23c65d9315cf9a0639657edfd3db6a
miloh1elena <Administrator@PROF25.xytech.xytechsystems.com>
2023-11-22 19:53:38 -0500

    attempt 7 for js fix

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 import numpy as np
 import pandas as pd
@@ -7,98 +6,14 @@ import base64
 import plotly.express as px
 from io import BytesIO
 import plotly.io as pio
-from flask_cors import CORS  # mport for CORS support
-=======
-# from flask import Flask, render_template, jsonify, request
-# import pandas as pd
-# import numpy as np
-# from flask_cors import CORS
-# import os
-
-# app = Flask(__name__)
-# CORS(app)
-
-# @app.route('/get_plot_data')
-# def get_plot_data():
-#     # Your logic to generate plot data
-#     fig = go.Figure(data=[go.Scatter(x=[1, 2, 3], y=[4, 5, 6])])  # Example plot
-#     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-#     return graphJSON
-
-# # Set up the static folder path
-# static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
-
-# # Load BTC data
-# data = pd.read_csv('bitcoin_trading_data.csv', index_col=0)
-# next_day_formatted = pd.to_datetime(data.index[-1]) + pd.Timedelta(days=1)
-
-# # Load model predictions
-# predicted_close_price = np.nan  # Replace this with the actual value
-
-# # Function to fetch BTC data
-# def fetch_btc_data(selected_date):
-#     try:
-#         # Modify data retrieval based on the selected date if needed
-#         historical_close_prices = data[['Close']].tail(60).reset_index().to_dict('records')
-#         volatility_data = data[['30d_Rolling_Std']].tail(60).reset_index().to_dict('records')
-
-#         return {
-#             'historicalClosePrices': historical_close_prices,
-#             'volatilityData': volatility_data,
-#             'additionalInfo': {
-#                 'next_day': str(next_day_formatted.date()),
-#                 'predicted_close_price': predicted_close_price
-#             }
-#         }
-#     except Exception as e:
-#         print(f"Error fetching BTC data: {e}")
-#         return {'error': str(e)}
-
-# # Flask route to render the HTML page
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# # Flask route to serve BTC data as JSON
-# @app.route('/api/btcdata')
-# def btc_data():
-#     selected_date = request.args.get('date', default='2023-01-01')  # Default date if not provided
-#     data = fetch_btc_data(selected_date)
-
-#     if 'error' in data:
-#         return jsonify({'error': data['error']}), 500
-
-#     return jsonify(data)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-from flask import Flask, render_template, jsonify, request
-import pandas as pd
-import numpy as np
-from flask_cors import CORS
-from flask.json import JSONEncoder
-import os
-
-# Custom JSON encoder to handle NaNs
-class CustomJSONEncoder(JSONEncoder):
-    def default(self, obj):
-        if pd.isna(obj):
-            return None  # Convert NaN to None
-        return super().default(obj)
-
-app = Flask(__name__)
-app.json_encoder = CustomJSONEncoder
-CORS(app)
->>>>>>> a465ee533b423e11ac9837672ecf4ec3bc30db8c
+from flask_cors import CORS  # Import for CORS support
+from datetime import datetime as dt 
+from datetime import date
 
 # Load BTC data
 data = pd.read_csv('bitcoin_trading_data.csv', index_col=0)
-data.index = pd.to_datetime(data.index)
-next_day_formatted = data.index[-1] + pd.Timedelta(days=1)
+next_day_formatted = pd.to_datetime(data.index[-1]) + pd.Timedelta(days=1)
 
-<<<<<<< HEAD
 # Load model predictions
 predicted_close_price = np.nan  # actual value????
 
@@ -128,7 +43,7 @@ def fetch_btc_data(selected_date):
 
         # Ensure 'additionalInfo' key is always present
         data['additionalInfo'] = {
-            'next_day': str(next_day_formatted.date()),
+            'next_day': next_day_formatted.strftime("%Y-%m-%d"),
             'predicted_close_price': predicted_close_price,
             'volatilityPlot': volatility_plot_data,
             'boxPlot': box_plot_data
@@ -149,25 +64,20 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 # Flask route to render the main HTML page
-=======
-# Generate dummy prediction (Replace with actual model prediction)
-predicted_close_price = 40000  # Example prediction
-
->>>>>>> a465ee533b423e11ac9837672ecf4ec3bc30db8c
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Flask route to serve BTC data as JSON
 @app.route('/api/btcdata')
 def btc_data():
-<<<<<<< HEAD
     selected_date = request.args.get('date', default='2023-11-11')  # Default date if not selected
-    data = fetch_btc_data(selected_date)
+    data_result = fetch_btc_data(selected_date)
 
-    if 'error' in data:
-        return jsonify({'error': data['error']}), 500
+    if 'error' in data_result:
+        return jsonify({'error': data_result['error']}), 500
 
-    return jsonify(data)
+    return jsonify(data_result)
 
 # Flask route to serve volatility chart as HTML
 @app.route('/volatility_chart')
@@ -197,26 +107,3 @@ def correlation_heatmap():
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-    # Historical prices
-    historical_close_prices = data[['Close']].tail(60).reset_index().to_dict('records')
-    
-    # Rolling volatility
-    volatility_data = data['Close'].rolling(30).std().tail(60).reset_index().to_dict('records')
-    
-    # Correlation matrix
-    corr_matrix = data.corr().reset_index().melt('index').to_dict('records')
-    
-    # Return all data
-    response_data = {
-        'historicalClosePrices': historical_close_prices,
-        'volatilityData': volatility_data,
-        'corrMatrix': corr_matrix,
-        'predictedClosePrice': predicted_close_price,
-        'nextDay': str(next_day_formatted.date())
-    }
-    return jsonify(response_data)
-
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> a465ee533b423e11ac9837672ecf4ec3bc30db8c
